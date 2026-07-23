3e5ba4dfa2b06c64391fc23ca6cffbfd1d8cb2ac
PavanKumar-2 <20h65a0209@cvsr.ac.in>
2026-07-23 22:51:40 +0530

    Solved selection sort

---
+++ Sorting/SelectionSorting.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class SelectionSorting {
+
+    public static void selectionSort(int[] nums){
+        int i=0;
+        while(i<nums.length-1){
+            int j = i+1;
+            int ind = i;
+            int min = nums[i];
+            while(j<nums.length){
+                if(nums[j]<min){
+                    min = nums[j];
+                    ind =j ;
+                }
+                j+=1;
+            }
+            int temp = nums[ind];
+            nums[ind]=nums[i];
+            nums[i]=temp;
+            i+=1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{25,31,18,30,23,26,25};
+        selectionSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+}
