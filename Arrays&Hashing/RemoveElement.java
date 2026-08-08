deb2e40a941b8ea0ca9968ded45b1d831fa18050
Pavan Bommedi <pavanbommedi25@gmail.com>
2026-08-08 13:39:59 +0530

    solving remove elements

---
+++ Arrays&Hashing/RemoveElement.java
@@ -0,0 +1,19 @@
+public class RemoveElement{
+    public static int removeElement(int[] nums, int val) {
+        int i =0;
+        for(int j = 0;j<nums.length;j++){
+            if(nums[j]!=val){
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        return i;
+
+    }
+    public static void main(String[] args){
+        int[] nums = new int[]{0,1,2,3,4,3,4};
+        int val = 3;
+        System.out.println(removeElement(nums,val));
+
+    }
+}
